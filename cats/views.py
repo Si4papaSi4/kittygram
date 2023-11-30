a63ace0e4d155c2becfbf5351315fc80936a7c8d
TAT-PRINCE <ypr.nroclk@ynadex.ru>
2023-11-30 12:52:30 +0300

    Save

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,47 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+
+
+class APICatDetail(APIView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cat = get_object_or_404(Cat, id=kwargs.get('pk'))
+    def get(self, request, **kwargs):
+        serializer = CatSerializer(self.cat)
+        return Response(serializer.data)
+
+    def put(self, request, **kwargs):
+        serializer = CatSerializer(self.cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, **kwargs):
+        return self.put(request)
+
+    def delete(self, request, **kwargs):
+        self.cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
